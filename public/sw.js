d0fc87fbebe3714f2a4b0ddd24072c71d3de2471
datahorizonmodz <maderedanayasaa@gmail.com>
2026-07-06 08:40:29 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
+// service worker
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 self.addEventListener("fetch", () => {});
